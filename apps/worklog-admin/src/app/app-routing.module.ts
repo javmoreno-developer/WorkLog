a96a746628db7782084824fe0e1b6a583d86acf7
javmoreno <javmoreno766@gmail.com>
2023-06-10 13:09:51 +0200

    refactor(re-01): refactor admin app

--- apps/worklog-admin/src/app/app-routing.module.ts
+++ apps/worklog-admin/src/app/app-routing.module.ts
@@ -23,52 +23,62 @@ const routes: Routes = [
   {
     path: "modules",
     loadChildren: () =>
-      import("./pages/modules/modules.module").then((m) => m.ModulesPageModule)
+      import("./pages/modules/modules.module").then((m) => m.ModulesPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "students",
     loadChildren: () =>
-      import("./pages/students/students.module").then((m) => m.StudentsPageModule)
+      import("./pages/students/students.module").then((m) => m.StudentsPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "student/entries",
     loadChildren: () =>
-      import("./pages/entries/entries.module").then((m) => m.EntriesPageModule)
+      import("./pages/entries/entries.module").then((m) => m.EntriesPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "student/entry/comment",
     loadChildren: () =>
-      import("./pages/comments/comments.module").then((m) => m.CommentsPageModule)
+      import("./pages/comments/comments.module").then((m) => m.CommentsPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "agreements",
     loadChildren: () =>
-      import("./pages/agreements/agreements.module").then((m) => m.AgreementsPageModule)
+      import("./pages/agreements/agreements.module").then((m) => m.AgreementsPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "companies",
     loadChildren: () =>
-      import("./pages/companies/companies.module").then((m) => m.CompaniesPageModule)
+      import("./pages/companies/companies.module").then((m) => m.CompaniesPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "units",
     loadChildren: () =>
-      import("./pages/units/units.module").then((m) => m.UnitsPageModule)
+      import("./pages/units/units.module").then((m) => m.UnitsPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "teachers",
     loadChildren: () =>
-      import("./pages/teachers/teachers.module").then((m) => m.TeachersPageModule)
+      import("./pages/teachers/teachers.module").then((m) => m.TeachersPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "profile",
     loadChildren: () =>
-      import("./pages/profiles/profiles.module").then((m) => m.ProfilesPageModule)
+      import("./pages/profiles/profiles.module").then((m) => m.ProfilesPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "labors",
     loadChildren: () =>
-      import("./pages/labors/labors.module").then((m) => m.LaborsPageModule)
+      import("./pages/labors/labors.module").then((m) => m.LaborsPageModule),
+      canActivate: [AuthGuard]
   },
   {
     path: "",
